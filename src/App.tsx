a22daf76d8ec6ba250dc2da69cb143f0a628cf22
Hiroki Asano <asano.hiroki@gmail.com>
2024-12-08 02:04:56 +0900

    fix router to get route on GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
 import OlelTop from "./OlelTop";
